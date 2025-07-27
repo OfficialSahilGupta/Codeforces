25db82f55998c1b4245fc92fa12d99cc565a4608
Sahil Gupta <sahilgupta98122@gmail.com>
2025-07-27 10:31:07 +0530

    Update WhoOpposite.java

--- rating 800/Maths/WhosOpposite.java
+++ rating 800/Maths/WhosOpposite.java
@@ -1,26 +1,36 @@
 import java.util.Scanner;
 
 public class WhosOpposite {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        for (int i = 0; i < t; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int c = scanner.nextInt();
-            int diff = Math.abs(a - b);
-            int n = 2 * diff;
-            if (Math.max(a, b) < 2 * Math.min(a, b)) {
-                System.out.println(-1);
-                continue;
-            }
-            if (c > n || c < 1) {
-                System.out.println(-1);
-                continue;
-            }
-            int d = (c + diff - 1) % n + 1;
-            System.out.println(d);
+    static Scanner sc = new Scanner(System.in);
+
+    static int ans(){
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int diff = Math.abs(a - b);
+        int n = 2 * diff;
+
+        if(Math.max(a, b) < 2 * Math.min(a, b)){
+            return -1;
+            // continue;
         }
-        scanner.close();
+
+        if(c > n || c < 1){
+            return -1;
+            // continue;
+        }
+
+        int d = (c + diff - 1) % n + 1;
+        return d;
+
+    }
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+        while(t-- > 0){
+            System.out.println(ans());
+        }
+
+        sc.close();
     }
 }
