57d79789d1246bc44e4f2f70b311e2f5ee81fe2a
Sahil Gupta <sahilgupta98122@gmail.com>
2025-09-07 22:28:34 +0530

    Modify RestoringThreeNumbers.java

--- rating 800/Maths/RestoringThreeNumbers.java
+++ rating 800/Maths/RestoringThreeNumbers.java
@@ -17,10 +17,20 @@ public class RestoringThreeNumbers{
     }
 
     static int solve() throws IOException{
-        
+    static void solve() throws IOException{
+        String[] parts = br.readLine().trim().split(" ");
+        int[] arr = new int[4];
+
+        for(int i = 0; i < 4; i++){
+            arr[i] = Integer.parseInt(parts[i]);
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println((arr[3] - arr[2]) + " " + (arr[3] - arr[1]) + " " + (arr[3] - arr[0]));
     }
 
     public static void main(String[] args) throws IOException{
-        System.out.println(solve());
+        solve();
     }
 }
