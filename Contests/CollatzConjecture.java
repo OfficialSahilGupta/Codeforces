6652af5fb71072e8b8e8a0ba23d09138f1a6e9c5
Sahil Gupta <sahilgupta98122@gmail.com>
2025-09-07 20:48:29 +0530

    Add CollatzConjecture.java

---
+++ Contests/CollatzConjecture.java
@@ -0,0 +1,33 @@
+// codeforces round 1047 div 3
+
+import java.util.*;
+import java.io.*;
+
+public class CollatzConjecture{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException{
+        while(st == null || !st.hasMoreTokens()){
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException{
+        return Integer.parseInt(next());
+    }
+
+    static int solve() throws IOException{
+        int k = nextInt(), x = nextInt();
+
+        return x << k;
+    }
+
+    public static void main(String[] args) throws IOException{
+        int t = nextInt();
+        while(t-- > 0){
+            System.out.println(solve());
+        }
+    }
+}
