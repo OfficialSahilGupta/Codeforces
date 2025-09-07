4b39a3842b84f70e80911e7935379029db80786c
Sahil Gupta <sahilgupta98122@gmail.com>
2025-09-07 21:55:14 +0530

    Create RestoringThreeNumbers.java

---
+++ rating 800/Maths/RestoringThreeNumbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class RestoringThreeNumbers{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException{
+        while(st == null || !st.hasMoreTokens()){
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException{
+        return Integer.parseInt(next());
+    }
+
+    static int solve() throws IOException{
+        
+    }
+
+    public static void main(String[] args) throws IOException{
+        System.out.println(solve());
+    }
+}
