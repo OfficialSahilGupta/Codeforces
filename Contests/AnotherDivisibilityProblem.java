6501b560b32f002efaf8be77c65e02829f04b49a
Sahil Gupta <sahilgupta98122@gmail.com>
2025-09-09 21:28:42 +0530

    Add AnotherDivisibilityProblem.java

---
+++ Contests/AnotherDivisibilityProblem.java
@@ -0,0 +1,32 @@
+// codeforces round 1049 div 2
+
+import java.util.*;
+import java.io.*;
+
+public class  AnotherDivisibilityProblem{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException{
+        while(st == null || !st.hasMoreTokens()){
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException{
+        return Integer.parseInt(next());
+    }
+
+    static int solve() throws IOException{
+        int x = nextInt();
+        return (x * 8);
+    }
+
+    public static void main(String[] args) throws IOException{
+        int t = nextInt();
+        while(t-- > 0){
+            System.out.println(solve());
+        }
+    }
+}
