c41dbb8033d83757078e526dfc676e9daf0acdca
Sahil Gupta <sahilgupta98122@gmail.com>
2025-10-24 20:33:35 +0530

    Add contest question PizzaTime.java

---
+++ Contests/PizzaTime.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class PizzaTime{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException{
+        while(st == null || !st.hasMoreTokens()){
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException{
+        return Integer.parseInt(next());
+    }
+
+    static int solution() throws IOException{
+        int n = nextInt();
+        return ((n - 1) / 2);
+    }
+
+    public static void main(String[] args) throws IOException{
+        int t = nextInt();
+        while(t-- > 0){
+            System.out.println(solution());
+        }
+    }
+}
