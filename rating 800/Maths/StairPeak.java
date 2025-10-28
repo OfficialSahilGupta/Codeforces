06f35fb6af3e17b1ffdc4af438fa3f177cd0ab5f
Sahil Gupta <sahilgupta98122@gmail.com>
2025-10-28 17:08:13 +0530

    Add StairPeak.java

---
+++ rating 800/Maths/StairPeak.java
@@ -0,0 +1,24 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class StairPeak{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            if(a < b && b < c && a < c){
+                System.out.println("STAIR");
+            }
+            else if(a < b && b > c){
+                System.out.println("PEAK");
+            }
+            else System.out.println("NONE");
+        }
+        sc.close();
+    }
+}
