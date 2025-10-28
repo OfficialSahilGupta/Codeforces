bfcc74bf694809f543ec87aa48badc3d7185ba8d
Sahil Gupta <sahilgupta98122@gmail.com>
2025-10-28 21:45:21 +0530

    Add Square.java

---
+++ Contests/Square.java
@@ -0,0 +1,22 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class Square{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+            if(a == b && b == c && c == d){
+                System.out.println("YES");
+            }
+            else System.out.println("NO");
+        }
+        
+        sc.close();
+    }
+}
