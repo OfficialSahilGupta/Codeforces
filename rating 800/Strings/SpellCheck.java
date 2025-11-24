833f21c0432c7a180c7ffce0802c13c065ea940d
Sahil Gupta <sahilgupta98122@gmail.com>
2025-11-24 23:59:44 +0530

    Add SpellCheck,java

---
+++ rating 800/Strings/SpellCheck.java
@@ -0,0 +1,28 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class SpellCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            if(n != 5){
+                System.out.println("NO");
+            }
+
+            else{
+                if(s.contains("T") && s.contains("i") && s.contains("m") && s.contains("u") && s.contains("r")){
+                    System.out.println("YES");
+                }
+                else System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
