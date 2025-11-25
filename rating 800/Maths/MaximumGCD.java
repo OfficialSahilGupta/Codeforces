178c7c06062ba0655dccb6ecaa52faf8b82c512b
Sahil Gupta <sahilgupta98122@gmail.com>
2025-11-25 09:45:18 +0530

    Add MaximumGCD.java

---
+++ rating 800/Maths/MaximumGCD.java
@@ -0,0 +1,18 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class MaximumGCD{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            System.out.println(n/ 2);
+        }
+        sc.close();
+    }
+}
