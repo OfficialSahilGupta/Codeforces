38a96b4ef862ce25dec98a80e3240daaad1fb566
Sahil Gupta <sahilgupta98122@gmail.com>
2026-01-01 01:47:12 +0530

    Add LineTrip.java

---
+++ rating 800/Maths/LineTrip.java
@@ -0,0 +1,28 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class LineTrip{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt(), x = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+            int answer = arr[0];
+            for(int i = 1; i < n; i++){
+                answer = Math.max(answer, arr[i] - arr[i - 1]);
+            }
+
+            answer = Math.max(answer, 2 * (x - arr[n - 1]));
+            System.out.println(answer);
+        }
+        sc.close();
+    }
+}
