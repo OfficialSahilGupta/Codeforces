c2343b20f14b7aeeb4353669558261ca83db24b2
Sahil Gupta <sahilgupta98122@gmail.com>
2026-01-02 12:44:21 +0530

    Add MyFirstSortingProblem.java

---
+++ rating 800/Maths/MyFirstSortingProblem.java
@@ -0,0 +1,21 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class MyFirstSortingProblem{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int x = sc.nextInt(), y = sc.nextInt();
+            if(x <= y){
+                System.out.println(x + " " + y);
+            }
+            else System.out.println(y + " " + x);
+        }
+        sc.close();
+    }
+}
