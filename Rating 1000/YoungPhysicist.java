e9a0c631c992b900270194f35392f1125cf74400
Sahil Gupta <sahilgupta98122@gmail.com>
2026-01-08 10:09:45 +0530

    Add YoungPhysicist.java

---
+++ Rating 1000/YoungPhysicist.java
@@ -0,0 +1,22 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class YoungPhysicist{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum1 = 0, sum2 = 0, sum3 = 0;
+        for(int i = 0; i < n; i++){
+            sum1 += sc.nextInt();
+            sum2 += sc.nextInt();
+            sum3 += sc.nextInt();
+        }
+        if(sum1 == 0 && sum2 == 0 && sum3 == 0)System.out.println("YES");
+        else System.out.println("NO");
+        sc.close();
+    }
+}
---
+++ rating 800/Maths/TwoArraysAndSwaps.java
@@ -0,0 +1,35 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class TwoArraysAndSwaps{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt(), k = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            for(int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for(int i = 0; i < n; i++) b[i] = sc.nextInt();
+
+            Arrays.sort(a);
+            Arrays.sort(b);
+
+            int sum = 0;
+            int temp = k;
+            for(int i = 0; i < temp; i++){
+                if(a[i] > b[i]){
+                    sum += a[i];
+                }
+                else sum += b[i];
+            }
+
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
