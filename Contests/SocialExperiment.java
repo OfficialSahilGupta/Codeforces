aa40e4f7e3915e241c6dcd3164d9548c4d71f965
Sahil Gupta <sahilgupta98122@gmail.com>
2026-01-17 22:13:17 +0545

    Add SocialExperiment.java

---
+++ Contests/SocialExperiment.java
@@ -0,0 +1,21 @@
+/**
+ * author: TheSahilGupta
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class SocialExperiment{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            if(n == 2)System.out.println(n);
+            else if(n == 3) System.out.println(n);
+            else if(n % 2 == 0)System.out.println(0);
+            else System.out.println(1);
+        }
+        sc.close();
+    }
+}
